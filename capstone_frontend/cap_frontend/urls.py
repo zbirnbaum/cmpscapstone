0d47768939509958d0d8f046fc75c7256ee72f16
cecehaase <150824347+cecehaase@users.noreply.github.com>
2025-02-06 09:21:04 -0600

    Update urls.py

--- capstone_frontend/cap_frontend/urls.py
+++ capstone_frontend/cap_frontend/urls.py
@@ -21,5 +21,7 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
+
 ]
