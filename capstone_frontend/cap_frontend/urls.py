78470891871e33e4dfdaff12ea3fea13ffc13f6f
dtcicero <130113355+dtcicero@users.noreply.github.com>
2025-02-16 13:45:58 -0600

    Update urls.py
    Updating analytics path

--- capstone_frontend/cap_frontend/urls.py
+++ capstone_frontend/cap_frontend/urls.py
@@ -22,6 +22,7 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', views.index, name='index'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('analytics/', views.analytics, name='analytics'), 
 
 ]
